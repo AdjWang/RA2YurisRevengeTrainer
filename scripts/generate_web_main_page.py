4fc43d7bfd9ce2bae9b8ca505b77c98043f17abb
AdjWang <wwang230513@gmail.com>
2025-06-10 23:52:23 +0800

    fix web page generator encoding

--- scripts/generate_web_main_page.py
+++ scripts/generate_web_main_page.py
@@ -18,9 +18,9 @@ if __name__ == '__main__':
     if not Path.exists(WEB_DIST_PATH):
         print(f'Failed to find dist dir={WEB_DIST_PATH}, build web first')
         sys.exit(1)
-    with open(WEB_DIST_PATH, 'r') as f:
+    with open(WEB_DIST_PATH, 'r', encoding='utf-8') as f:
         html_content = f.read()
     cpp_content = CPP_TEMPLATE.format(html_content)
-    with open(OUTPUT_PATH, 'w') as f:
+    with open(OUTPUT_PATH, 'w', encoding='utf-8') as f:
         f.write(cpp_content)
     
