f946af60ba2dc96ebd3d6f77c38f103bbdae842a
AdjWang <wwang230513@gmail.com>
2026-03-23 23:40:18 +0800

    Add logging.

--- src/frontend/ra2_trainer/src-tauri/src/lib.rs
+++ src/frontend/ra2_trainer/src-tauri/src/lib.rs
@@ -1,13 +1,16 @@
+use global_hotkey::HotKeyState;
+use log::{debug, error, info};
 use once_cell::sync::OnceCell;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs;
+use std::path::PathBuf;
 use std::str::FromStr;
 use std::sync::Mutex;
 use tauri::Emitter;
 use tauri::Manager;
 use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers, Shortcut};
-use global_hotkey::{HotKeyState};
+use tauri_plugin_log::{Target, TargetKind};
 
 #[derive(Default)]
 struct AppState {
@@ -27,6 +30,12 @@ struct Ra2TrainerConfig {
 }
 
 #[tauri::command]
+fn get_ws_port(state: tauri::State<'_, Mutex<AppState>>) -> u16 {
+    let state = state.lock().unwrap();
+    state.config.get().unwrap().ra2_trainer.port
+}
+
+#[tauri::command]
 fn get_label_hotkey_string(state: tauri::State<'_, Mutex<AppState>>, label_name: &str) -> String {
     // Get key code from config.
     let state = state.lock().unwrap();
@@ -41,7 +50,7 @@ fn read_config() -> Result<Config, String> {
     let exe_path = std::env::current_exe().map_err(|e| format!("Failed to get exe path: {}", e))?;
     let config_path = exe_path.parent().unwrap().join("ra2_trainer.toml");
     // Read and parse config file.
-    println!("Loading config from {:?}", config_path);
+    info!("Loading config from {:?}", config_path);
     let config_content = match fs::read_to_string(&config_path) {
         Ok(content) => content,
         Err(e) => {
@@ -57,7 +66,10 @@ fn read_config() -> Result<Config, String> {
     }
 }
 
-fn register_hotkeys(app: &tauri::App, hotkeys: &HashMap<String, String>) -> HashMap<String, String> {
+fn register_hotkeys(
+    app: &tauri::App,
+    hotkeys: &HashMap<String, String>,
+) -> HashMap<String, String> {
     #[cfg(desktop)]
     {
         let mut registered_hotkeys = HashMap::new();
@@ -65,19 +77,29 @@ fn register_hotkeys(app: &tauri::App, hotkeys: &HashMap<String, String>) -> Hash
         for (label_name, key_name) in hotkeys {
             let key_code: Code = Code::from_str(key_name).unwrap();
             let shortcut = Shortcut::new(Some(Modifiers::ALT), key_code);
-            match app.global_shortcut().on_shortcut(shortcut, move |app_handle , shortcut, event| {
-                if event.state == HotKeyState::Pressed {
-                    println!("Hotkey event for label '{}': {:?}", shortcut.key, event);
-                    app_handle.emit("hotkey-pressed", serde_json::json!({
-                        "labelName": format!("{}", shortcut.key),
-                    })).unwrap();
-                }
-            }) {
+            match app
+                .global_shortcut()
+                .on_shortcut(shortcut, move |app_handle, shortcut, event| {
+                    if event.state == HotKeyState::Pressed {
+                        debug!("Hotkey event for label '{}': {:?}", shortcut.key, event);
+                        app_handle
+                            .emit(
+                                "hotkey-pressed",
+                                serde_json::json!({
+                                    "labelName": format!("{}", shortcut.key),
+                                }),
+                            )
+                            .unwrap();
+                    }
+                }) {
                 Ok(_) => {
                     registered_hotkeys.insert(label_name.clone(), key_name.clone());
-                    println!("Registered hotkey for label '{}': {}", label_name, key_name);
-                },
-                Err(e) => println!("Failed to register hotkey for label '{}': {}", label_name, e),
+                    debug!("Registered hotkey for label '{}': {}", label_name, key_name);
+                }
+                Err(e) => error!(
+                    "Failed to register hotkey for label '{}': {}",
+                    label_name, e
+                ),
             }
         }
         registered_hotkeys
@@ -90,9 +112,22 @@ pub fn run() {
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .invoke_handler(tauri::generate_handler![get_label_hotkey_string])
         .setup(move |app: &mut tauri::App| {
-            assert!(app.manage(Mutex::new(AppState{
-                    config: OnceCell::new(),
-                    registered_hotkeys: HashMap::new(),
+            let app_path = std::env::current_exe().unwrap();
+            let app_dir = app_path.parent().unwrap();
+            app.handle().plugin(
+                tauri_plugin_log::Builder::new()
+                    .targets([
+                        Target::new(TargetKind::Stdout),
+                        Target::new(TargetKind::Folder {
+                            path: PathBuf::from(app_dir),
+                            file_name: Some("ra2_trainer_frontend.log".to_string()),
+                        }),
+                    ])
+                    .build(),
+            ).unwrap();
+            assert!(app.manage(Mutex::new(AppState {
+                config: OnceCell::new(),
+                registered_hotkeys: HashMap::new(),
             })));
             let state = app.handle().state::<Mutex<AppState>>();
             let mut state_guard = state.lock().unwrap();
@@ -107,11 +142,17 @@ pub fn run() {
                             register_hotkeys(app, &state_guard.config.get().unwrap().hotkeys);
 
                         // Pass the backend URL to the frontend.
-                        let _ = webview.eval(&format!("window.backendUrl = '{}'; window.waitBackend();", backend_url));
+                        let _ = webview.eval(&format!(
+                            "window.backendUrl = '{}'; window.waitBackend();",
+                            backend_url
+                        ));
                     }
                     Err(e) => {
-                        let _ = webview.eval(&format!("window.errorMessage = '{}'; window.waitBackend();", e));
-                    },
+                        let _ = webview.eval(&format!(
+                            "window.errorMessage = '{}'; window.waitBackend();",
+                            e
+                        ));
+                    }
                 };
             }
             Ok(())
